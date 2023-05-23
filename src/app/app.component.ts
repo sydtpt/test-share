b2bc9b2771c0b112bf3202f8e15d33e16307c441
Sydnei Peixoto Tanikawa <sydtpt@gmail.com>
2023-05-23 10:12:19 +0200

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {Meta} from "@angular/platform-browser";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'test-share';
+
+  constructor(private metaService: Meta) {
+    setTimeout(() => {
+        this.metaService.updateTag({name: "twitter:card", content: "summary"});
+        this.metaService.updateTag({name: "twitter:site", content: "@IPEX"});
+        this.metaService.updateTag({name: "twitter:creator", content: "@IPEX"});
+        this.metaService.updateTag({name: "twitter:title", content: "IPEX"});
+        this.metaService.updateTag({name: "twitter:description", content: "IPEX, Interparliamentary, Exchange, National Parliaments, European Parliament, Treaty of Lisbon"});
+        this.metaService.updateTag({name: "twitter:image", content: "https://ipexlpp.secure.europarl.europa.eu/IPEXL-WEB/assets/sharing-img.jpg"});
+    }, 200);
+  }
+
+
+
 }
